b8211137c2ed3160c08182bafbaa518ee4d676ce
hsrsbd <hsraffety@gmail.com>
2019-06-25 21:08:47 -0700

    updating routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // dependencies
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { MDBContainer } from 'mdbreact';
 
 // components
 import Dashboard from './components/Dashboard';
@@ -11,17 +10,9 @@ import Register from './components/Register';
 import Login from './components/Login';
 import PrivateRoute from './components/PrivateRoute';
 import Items from './components/Items';
+import Item from './components/Item';
 // styles
 import './App.scss';
-
-const Item = props => {
-  console.log(`item props`, props);
-  return (
-    <div className='test-div'>
-      <h1>aasdasd</h1>
-    </div>
-  );
-};
 
 class App extends Component {
   render() {
@@ -31,6 +22,7 @@ class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register} />
+        <Route path='/items' component={Items} />
         <PrivateRoute path='/item/:id' component={Item} />
         <PrivateRoute path='/dashboard' component={Dashboard} />;
       </Router>
--- src/components/Items.js
+++ src/components/Items.js
@@ -12,6 +12,7 @@ class Items extends Component {
   }
 
   render() {
+    console.log(this.props);
     return (
       <div className='items-container'>
         <div className='cards'>
